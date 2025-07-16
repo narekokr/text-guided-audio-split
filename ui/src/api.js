7e14886faa9c587626321276a80e67c18744f053
narek <narek.okroyan@outlook.com>
2025-07-16 21:34:18 +0200

    fix requirements

--- ui/src/api.js
+++ ui/src/api.js
@@ -1,5 +1,5 @@
 // api.js
-const API_URL = "http://localhost:8000";
+const API_URL = process.env.REACT_APP_FIREBASE_API_URL;
 
 export const uploadAudio = async (file, sessionId, userId) => {
     const formData = new FormData();
